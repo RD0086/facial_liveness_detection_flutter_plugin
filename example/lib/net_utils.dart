b8cf7692a747714a362a569fdfded4c5777a8fa5
ReidLee <ruide.li@esandinfo.com>
2024-09-25 23:39:11 +0800

    update : update android sdk 1.17.0

--- example/lib/net_utils.dart
+++ example/lib/net_utils.dart
@@ -2,9 +2,17 @@ import 'dart:math';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-// * 注意，这段代码要写在服务器上，建议通过服务器把数据转发给网关，避免密钥泄露！
+/**
+ * 请求服务器
+ * 注意，这段代码要写在服务器上，建议通过服务器把数据转发给网关，避免密钥泄露！
+ * 网关的接入可参考文档：https://esandinfo.yuque.com/yv6e1k/aa4qsg/uu6e86
+ * 或者参考开源项目 (直接用即可)：
+ *    a. JAVA : https://gitee.com/szrd2023/es-gateway-sdk
+ *    b. PHP : https://gitee.com/szrd2023/es-gateway-sdk-php
+ *    c. Golang : https://gitee.com/szrd2023/es-gateway-sdk-go
+ */
 
-String APPCODE = ""; // 替换为你的APPCODE
+String APPCODE = "TODO"; // 替换为你的APPCODE
 String generateRandomNonce() {
   // 生成随机数作为 X-Ca-Nonce 的值
   final random = Random();
